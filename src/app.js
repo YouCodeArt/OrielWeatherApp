031a34a73575e9b9b0d314da180b6d33c4a850ff
YC Art <97965078+YouCodeArt@users.noreply.github.com>
2022-02-26 01:29:09 +0200

    Finalize Project
    Update to background image
    Set minimum and maximum height and width for the boxes (cards)
    Change links to show a different color

--- src/app.js
+++ src/app.js
@@ -61,7 +61,7 @@ function displayForecast(response) {
       forecastIconsHTML =
         forecastIconsHTML +
         `<img
-          src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png"/width="60"/><br/>`;
+          src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png"/width="57"/>`;
     }
   });
   forecastIconsHTML = forecastIconsHTML + ``;
