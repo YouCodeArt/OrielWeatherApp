150aed4cb4817f42ce0d0e3c3fda722b3242d0ee
YC Art <97965078+YouCodeArt@users.noreply.github.com>
2022-02-26 12:48:16 +0200

    Update Final Layout
    Update layout when the browser is in minimum size

--- src/app.js
+++ src/app.js
@@ -61,7 +61,7 @@ function displayForecast(response) {
       forecastIconsHTML =
         forecastIconsHTML +
         `<img
-          src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png"/width="57"/>`;
+          src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png"/width="52"/>`;
     }
   });
   forecastIconsHTML = forecastIconsHTML + ``;
